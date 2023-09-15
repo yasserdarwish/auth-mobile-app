4bde53ce858126cbc13679c6b2785ecc3ed4ed90
Yasser <mr.yasser82@gmail.com>
2023-09-15 05:09:10 +0300

    final touch

--- auth_mobile_app/lib/cubits/cubit/auth_cubit.dart
+++ auth_mobile_app/lib/cubits/cubit/auth_cubit.dart
@@ -1,8 +1,8 @@
+import 'package:auth_mobile_app/main.dart';
 import 'package:auth_mobile_app/models/user_model.dart';
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 part 'auth_state.dart';
 
@@ -10,11 +10,12 @@ class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
   bool isChecked = false;
 
-  void saveCrud(String username, String password) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+  void saveCrud(int id, String username, String email, String gender) async {
     if (isChecked) {
+      prefs.setInt("id", id);
       prefs.setString("username", username);
-      prefs.setString("password", password);
+      prefs.setString("email", email);
+      prefs.setString("gender", gender);
     } else {
       prefs.clear();
     }
@@ -28,7 +29,7 @@ class AuthCubit extends Cubit<AuthState> {
           options: Options(headers: {'Content-Type': 'application/json'}));
       Map<String, dynamic> userInfo = response.data;
       UserModel user = UserModel.fromJson(userInfo);
-      saveCrud(username, password);
+      saveCrud(user.id, user.username, user.email, user.gender);
       emit(AuthSuccess());
       return user;
     } catch (e) {
--- auth_mobile_app/lib/main.dart
+++ auth_mobile_app/lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:auth_mobile_app/cubits/cubit/auth_cubit.dart';
 import 'package:auth_mobile_app/views/login_view.dart';
+import 'package:auth_mobile_app/views/profile_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+late SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   runApp(const AuthMobileApp());
 }
 
 class AuthMobileApp extends StatelessWidget {
   const AuthMobileApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AuthCubit(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const LoginView(),
+        home: prefs.getInt('id') == null
+            ? LoginView()
+            : ProfileView(
+                username: prefs.getString('username')!,
+                email: prefs.getString('email')!,
+                gender: prefs.getString('gender')!),
         theme: ThemeData(fontFamily: 'Inter'),
       ),
     );
